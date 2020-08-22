de9bf1241208ccc5dd678a1e8e3cf933a573ccb4
Paul Huynh <paul.huynh@aircanada.ca>
2020-08-22 14:36:11 -0400

    first commit

---
+++ SpaceTaseristic/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  SpaceTaseristic
+//
+//  Created by Paul Huynh on 2020-08-22.
+//  Copyright © 2020 PXSDevelopment. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+
+}
